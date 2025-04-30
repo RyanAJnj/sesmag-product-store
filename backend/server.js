726173879bfcafbe3e65bb8c58c6b440aac512ae
RyanAJnj <ryanaj.nj@gmail.com>
2025-04-29 20:12:50 -0400

    Switched from bcrypt to bcryptjs for compatibility

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { aj } from './lib/arcjet.js';
 
