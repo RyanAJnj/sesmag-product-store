e1643a8ffdac52e6d8943fbc32377c44ff0ef5ef
RyanAJnj <ryanaj.nj@gmail.com>
2025-04-29 20:43:07 -0400

    Fix: cleaned up API URL usage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ function LoginPage() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/auth/login`,
+        `${import.meta.env.VITE_API_URL}api/auth/login`,
         form
       );
       localStorage.setItem("token", res.data.token);
