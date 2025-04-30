de5dd558a2203fd9df5014b85502e8bbc2921069
RyanAJnj <ryanaj.nj@gmail.com>
2025-04-29 20:46:18 -0400

    Fix token logic for Vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,37 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import LoginPage from "./pages/LoginPage";
-
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useThemeStore } from "./store/useThemeStore";
 import { Toaster } from "react-hot-toast";
 
 function App() {
   const { theme } = useThemeStore();
-  const token = localStorage.getItem("token");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, []);
 
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme={theme}
+    >
       {token && <Navbar />}
 
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={token ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/product/:id" element={token ? <ProductPage /> : <Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={token ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/product/:id"
+          element={token ? <ProductPage /> : <Navigate to="/login" />}
+        />
       </Routes>
 
       <Toaster />
